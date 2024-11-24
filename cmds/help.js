02f82e3398f49d6a00e3ed42674e93bd4c871f0d
Dgos-dev <donrel658@gmail.com>
2024-11-24 11:21:11 +0800

    help.js

--- cmds/help.js
+++ cmds/help.js
@@ -5,13 +5,13 @@ module.exports = {
     cooldown: 16,		
     execute(api, event, args, commands) {
         let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 𝚊𝚝 https://github.com/hardasf/OctoBotRemake . 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
-        helpMessage += '💮═══════════════💮\n';
+        helpMessage += '༒🟪═══════════════🟪༒\n';
         commands.forEach((command, name) => {
             helpMessage += `𝙽𝚊𝚖𝚎: ${name}\n`;
             helpMessage += `𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚝𝚒𝚘𝚗: ${command.description}\n`;
             helpMessage += `𝚁ole: ${command.role} | CD: ${command.cooldown}\n`;
             helpMessage += `Credits: ${command.credits}\n`;
-        helpMessage += '💮═══════════════💮\n';
+        helpMessage += '༒🟪═══════════════🟪༒\n';
         });
         helpMessage += '💬https://facebook.com/leechshares';
         api.sendMessage(helpMessage, event.threadID);
